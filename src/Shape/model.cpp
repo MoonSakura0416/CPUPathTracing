4ed6b3fa8ea4a970295c51a95b050e06fe5b4878
MoonSakura <lry990416@gmail.com>
2025-09-17 09:32:00 +0800

    feat: integrate rapidobj for model loading and parsing

--- src/Shape/model.cpp
+++ src/Shape/model.cpp
@@ -2,45 +2,53 @@
 
 #include "Shape/model.h"
 #include "Util/profile.h"
+#include <rapidobj/rapidobj.hpp>
 
 Model::Model(const std::filesystem::path& path)
 {
     PROFILE("Load model" + path.string())
-    std::vector<glm::vec3> positions;
-    std::vector<glm::vec3> normals;
 
-    std::ifstream file(path);
-    if (!file.is_open()) {
-        std::println(std::cerr, "Failed to open file: {}", path.string());
-        return;
-    }
+    auto result = rapidobj::ParseFile(path, rapidobj::MaterialLibrary::Ignore());
 
-    std::string line;
-    char        trash;
-    while (!file.eof()) {
-        std::getline(file, line);
-        std::istringstream iss(line);
+    for (const auto& shape : result.shapes) {
+        size_t indexOffset = 0;
+        for (size_t numFaceVertex : shape.mesh.num_face_vertices) {
+            if (numFaceVertex == 3) {
+                auto      index = shape.mesh.indices[indexOffset];
+                glm::vec3 pos0{result.attributes.positions[index.position_index * 3 + 0],
+                               result.attributes.positions[index.position_index * 3 + 1],
+                               result.attributes.positions[index.position_index * 3 + 2]};
+                index = shape.mesh.indices[indexOffset + 1];
+                glm::vec3 pos1{result.attributes.positions[index.position_index * 3 + 0],
+                               result.attributes.positions[index.position_index * 3 + 1],
+                               result.attributes.positions[index.position_index * 3 + 2]};
+                index = shape.mesh.indices[indexOffset + 2];
+                glm::vec3 pos2{result.attributes.positions[index.position_index * 3 + 0],
+                               result.attributes.positions[index.position_index * 3 + 1],
+                               result.attributes.positions[index.position_index * 3 + 2]};
 
-        if (line.starts_with("v ")) {
-            glm::vec3 position;
-            iss >> trash >> position.x >> position.y >> position.z;
-            positions.push_back(position);
-        } else if (line.starts_with("vn ")) {
-            glm::vec3 normal;
-            iss >> trash >> trash >> normal.x >> normal.y >> normal.z;
-            normals.push_back(normal);
-        } else if (line.starts_with("f ")) {
-            // f 1//3 2//3 3//3
-            glm::ivec3 vIdx, vnIdx;
-            iss >> trash;
-            iss >> vIdx.x >> trash >> trash >> vnIdx.x;
-            iss >> vIdx.y >> trash >> trash >> vnIdx.y;
-            iss >> vIdx.z >> trash >> trash >> vnIdx.z;
-            triangles_.emplace_back(positions[vIdx.x - 1], positions[vIdx.y - 1],
-                                    positions[vIdx.z - 1], normals[vnIdx.x - 1],
-                                    normals[vnIdx.y - 1], normals[vnIdx.z - 1]);
+                if (index.normal_index >= 0) {
+                    index = shape.mesh.indices[indexOffset];
+                    glm::vec3 normal0{result.attributes.normals[index.normal_index * 3 + 0],
+                                      result.attributes.normals[index.normal_index * 3 + 1],
+                                      result.attributes.normals[index.normal_index * 3 + 2]};
+                    index = shape.mesh.indices[indexOffset + 1];
+                    glm::vec3 normal1{result.attributes.normals[index.normal_index * 3 + 0],
+                                      result.attributes.normals[index.normal_index * 3 + 1],
+                                      result.attributes.normals[index.normal_index * 3 + 2]};
+                    index = shape.mesh.indices[indexOffset + 2];
+                    glm::vec3 normal2{result.attributes.normals[index.normal_index * 3 + 0],
+                                      result.attributes.normals[index.normal_index * 3 + 1],
+                                      result.attributes.normals[index.normal_index * 3 + 2]};
+                    triangles_.emplace_back(pos0, pos1, pos2, normal0, normal1, normal2);
+                } else {
+                    triangles_.emplace_back(pos0, pos1, pos2);
+                }
+            }
+            indexOffset += numFaceVertex;
         }
     }
+
     build();
 }
 
